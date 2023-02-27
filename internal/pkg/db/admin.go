5fa27eef4e02a226fe76525a50de3cf3c15bf28c
Allen Kao <kaodondon7@gmail.com>
2023-02-28 02:29:44 +0800

    code refine

--- internal/pkg/db/admin.go
+++ internal/pkg/db/admin.go
@@ -5,14 +5,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func (c client) CreateAdmin(admin structs.Admin) (err error) {
+func (c *client) CreateAdmin(admin structs.Admin) (err error) {
 	log.Debug("create admin")
 
 	//_, err = c.admin.InsertOne(context.Background(), admin)
 	return
 }
 
-func (c client) GetAdmin() (data structs.Admin, err error) {
+func (c *client) GetAdmin() (data structs.Admin, err error) {
 	log.Debug("get admin")
 
 	//err = c.admin.FindOne(context.Background(), bson.M{}).Decode(&data)
