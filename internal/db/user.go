bfd94876f397182d4dc0cee56bb07a2010cf03d7
allenkao <kaodondon7@gmail.com>
2023-11-28 17:38:06 +0800

    refactor

--- internal/db/user.go
+++ internal/db/user.go
@@ -6,7 +6,7 @@ func (c *Client) ListFakeUsers(ctx context.Context) (data []string) {
 	log := c.log.WithContext(ctx)
 	log.Debug("list fake users")
 
-	if err := c.sql.core.WithContext(ctx).Raw("select username from template7.user where username like 'fakeuser%'").Scan(&data).Error; err != nil {
+	if err := c.sql.core.WithContext(ctx).Raw("select id from template7.user where username like 'fakeuser%'").Scan(&data).Error; err != nil {
 		c.log.WithError(err).Error("fail to get fake users")
 	}
 	return
