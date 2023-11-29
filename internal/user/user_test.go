c717d60ea8ecc1b7dcc489fa14c1aa6fc108277f
allenkao <kaodondon7@gmail.com>
2023-11-29 18:16:46 +0800

    add user transfer

--- internal/user/user_test.go
+++ internal/user/user_test.go
@@ -12,23 +12,39 @@ func TestNew(t *testing.T) {
 	viper.AddConfigPath("../../config")
 	ctx := context.WithValue(context.Background(), "traceId", uuid.NewString())
 
-	u := New(ctx, "fakeUser001", "fakeUser001")
-	u.GetWallet(ctx)
-	t.Log(u.String())
+	u1 := New(ctx, "fakeUser001", "fakeUser001")
+	t.Log(u1.String())
 
-	if err := u.Deposit(ctx, v1.Currency_usd.String(), 100); err != nil {
+	if err := u1.Deposit(ctx, v1.Currency_usd.String(), 100); err != nil {
 		t.Error(err)
 		return
 	}
 
-	u.GetWallet(ctx)
-	t.Log(u.String())
+	u1.GetWallet(ctx)
+	t.Log(u1.String())
 
-	if err := u.Withdraw(ctx, v1.Currency_usd.String(), 50); err != nil {
+	if err := u1.Withdraw(ctx, v1.Currency_usd.String(), 50); err != nil {
 		t.Error(err)
 		return
 	}
 
-	u.GetWallet(ctx)
-	t.Log(u.String())
+	u1.GetWallet(ctx)
+	t.Log(u1.String())
+
+	u2 := New(ctx, "fakeUser002", "fakeUser002")
+	t.Log(u2.String())
+
+	for wId, _ := range u2.wallets {
+		if err := u1.Transfer(ctx, wId, v1.Currency_usd.String(), 30); err != nil {
+			t.Error(err)
+			return
+		}
+		break
+	}
+
+	u1.GetWallet(ctx)
+	u2.GetWallet(ctx)
+
+	t.Log(u1.String())
+	t.Log(u2.String())
 }
